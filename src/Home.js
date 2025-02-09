e87c3853a105ed60759876ba90c560d99963c53b
In Hwa Mo <33287574+inhwamo@users.noreply.github.com>
2025-02-09 00:09:51 -0800

    finished 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+  const handleClick = (e) => {
+    //should log all properties of event if you want to use them
+    console.log("hello,ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello, " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("mario", e)}>
+        Click Me again
+      </button>
+      {/* This is a jsx comment. Cannot be handleClickAgain("mario") as that is function invocation syntax. It executes right away
+You have to create a function that will invoke on click, */}
     </div>
   );
 };
