303e8bb008084582ac70f87a2122e0a7a5d2c4cf
In Hwa Mo <33287574+inhwamo@users.noreply.github.com>
2025-03-11 23:56:16 -0700

    fix, also pass in title

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,11 @@
-const BlogList = ({ blogs }) => {
-  console.log(props, blogs);
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
+  // // don't need above
 
   return (
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
